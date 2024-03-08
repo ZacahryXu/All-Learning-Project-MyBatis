824572b34e5eb2e33f518ecbd680ce850a6f19f5
Zachary <1371938731@qq.com>
2024-03-08 22:57:24 +0800

    2438third commit

--- MyBatis-third/src/test/java/ResultMapTest.java
+++ MyBatis-third/src/test/java/ResultMapTest.java
@@ -4,6 +4,7 @@ import com.mybatis.utils.SqlSessionUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+
 import java.util.List;
 
 public class ResultMapTest {
@@ -19,8 +20,20 @@ public class ResultMapTest {
      *         <result property="age" column="age"></result>
      *         <result property="sex" column="sex"></result>
      *         <result property="email" column="email"></result>
+     *
      * </resultMap>
+     *
+     * 处理多对一的映射关系：
+     * a>级联属性赋值
+     * b>association
      */
+    @Test
+    public void testGetEmpAndDept(){
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.getEmpAndDept(1);
+        System.out.println(emp);
+    }
     @Test
     public void testGetAllEmp(){
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
